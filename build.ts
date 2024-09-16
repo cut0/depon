38d6ddff0b210a60fa9256ab7f4eca16d55e8381
Cut0 <59341902+cut0@users.noreply.github.com>
2024-09-17 04:06:08 +0900

    feat: setup cli publish (#17)

--- build.ts
+++ build.ts
@@ -1,8 +1,18 @@
 import { type BuildOptions, build } from "esbuild";
 
-// 共通のオプション
-const commonOptions: BuildOptions = {
+// core's common options
+const coreOptions: BuildOptions = {
   entryPoints: ["./src/core/index.ts"],
+  bundle: true,
+  minify: true,
+  sourcemap: false,
+  platform: "node",
+  target: "esnext",
+} as const;
+
+// cli's common options
+const cliOptions: BuildOptions = {
+  entryPoints: ["./src/cli/index.ts"],
   bundle: true,
   minify: true,
   sourcemap: false,
@@ -12,13 +22,18 @@ const commonOptions: BuildOptions = {
 
 Promise.all([
   build({
-    ...commonOptions,
+    ...coreOptions,
     format: "esm",
-    outfile: "./dist/index.mjs",
+    outfile: "./dist/core/index.mjs",
   }),
   build({
-    ...commonOptions,
+    ...coreOptions,
     format: "cjs",
-    outfile: "./dist/index.js",
+    outfile: "./dist/core/index.js",
+  }),
+  build({
+    ...cliOptions,
+    format: "esm",
+    outfile: "./dist/cli/index.mjs",
   }),
 ]).catch(() => process.exit(1));
