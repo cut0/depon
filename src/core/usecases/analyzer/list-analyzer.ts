82d1ffaf9f178f4c282acb748e89b9d261624136
cut0 <zeron9421@gmail.com>
2024-11-03 01:21:02 +0900

    feat: add depth option

--- src/core/usecases/analyzer/list-analyzer.ts
+++ src/core/usecases/analyzer/list-analyzer.ts
@@ -8,6 +8,7 @@ export type List = {
 export const getChildrenList = <T extends RelationNode>(
   relationList: T[],
   targetKey: string,
+  limitedDepth?: number,
 ): List => {
   const relationListWithoutLib = relationList.filter(
     (relation) =>
@@ -16,20 +17,24 @@ export const getChildrenList = <T extends RelationNode>(
   const visitedKeyMap: Record<string, boolean> = {};
   const childrenList: string[] = [];
 
-  const insertNode = (key: string) => {
+  const insertNode = (key: string, depth: number) => {
     visitedKeyMap[key] = true;
     childrenList.push(key);
+
+    if (limitedDepth != null && depth >= limitedDepth) {
+      return;
+    }
 
     const children = relationListWithoutLib
       .filter((item) => item.parent === key)
       .filter((relation) => !visitedKeyMap[relation.child]);
 
     for (const node of children) {
-      insertNode(node.child);
+      insertNode(node.child, depth + 1);
     }
   };
 
-  insertNode(targetKey);
+  insertNode(targetKey, 0);
 
   return [...new Set(childrenList)]
     .filter((key) => key !== targetKey)
@@ -39,6 +44,7 @@ export const getChildrenList = <T extends RelationNode>(
 export const getParentsList = <T extends RelationNode>(
   relationList: T[],
   targetKey: string,
+  limitedDepth?: number,
 ): List => {
   const relationListWithoutLib = relationList.filter(
     (relation) =>
@@ -47,20 +53,24 @@ export const getParentsList = <T extends RelationNode>(
   const visitedKeyMap: Record<string, boolean> = {};
   const parentsList: string[] = [];
 
-  const insertNode = (key: string) => {
+  const insertNode = (key: string, depth: number) => {
     visitedKeyMap[key] = true;
     parentsList.push(key);
+
+    if (limitedDepth != null && depth >= limitedDepth) {
+      return;
+    }
 
     const parents = relationListWithoutLib
       .filter((item) => item.child === key)
       .filter((relation) => !visitedKeyMap[relation.parent]);
 
     for (const node of parents) {
-      insertNode(node.parent);
+      insertNode(node.parent, depth + 1);
     }
   };
 
-  insertNode(targetKey);
+  insertNode(targetKey, 0);
 
   return [...new Set(parentsList)]
     .filter((key) => key !== targetKey)
